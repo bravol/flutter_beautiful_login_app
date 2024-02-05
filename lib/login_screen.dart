057b9f926ef70e2a29a1e9a650626f864fe03917
Lumala Brian <lumalabravo@gmail.com>
2024-02-05 15:38:44 +0300

    using blocconsumer instead of block builder and block listener, it is a combination of two

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -20,113 +20,107 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<AuthBloc, AuthState>(
-        listener: (context, state) {
-          // TODO: implement listener
-          if (state is AuthFailure) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.error),
-              ),
-            );
-          }
+      body: BlocConsumer<AuthBloc, AuthState>(listener: (context, state) {
+        // TODO: implement listener
+        if (state is AuthFailure) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.error),
+            ),
+          );
+        }
 
-          if (state is AuthSuccess) {
-            Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeScreen()),
-                (route) => false);
-          }
-        },
-        child: BlocBuilder<AuthBloc, AuthState>(
-          builder: (context, state) {
-            //loading state
-            if (state is AuthLoading) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            return SingleChildScrollView(
-              child: Center(
-                child: Column(
+        if (state is AuthSuccess) {
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+              (route) => false);
+        }
+      }, builder: (context, state) {
+        if (state is AuthLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 0),
+                  child: Image.asset('assets/images/signin_balls.png'),
+                ),
+                const Text(
+                  'Sign in',
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 50),
+                SocialButton(
+                  iconPath: 'assets/images/g_logo.svg',
+                  label: 'Continue with Google',
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 20),
+                SocialButton(
+                  iconPath: 'assets/images/f_logo.svg',
+                  label: 'Continue with Facebook',
+                  horizontalPadding: 80,
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  'or',
+                  style: TextStyle(fontSize: 17),
+                ),
+                const SizedBox(height: 15),
+                LoginField(
+                  hintiText: 'Email',
+                  controller: emailController,
+                ),
+                const SizedBox(height: 15),
+                LoginField(
+                  hintiText: 'Password',
+                  controller: passwordController,
+                ),
+                const SizedBox(height: 20),
+                GradientButton(
+                    onPressed: () {
+                      context.read<AuthBloc>().add(AuthLogginRequested(
+                          email: emailController.text.trim(),
+                          password: passwordController.text.trim()));
+                    },
+                    text: 'Sign in'),
+                const SizedBox(height: 30),
+                const Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 0),
-                      child: Image.asset('assets/images/signin_balls.png'),
-                    ),
-                    const Text(
-                      'Sign in',
-                      style:
-                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 50),
-                    SocialButton(
-                      iconPath: 'assets/images/g_logo.svg',
-                      label: 'Continue with Google',
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: 20),
-                    SocialButton(
-                      iconPath: 'assets/images/f_logo.svg',
-                      label: 'Continue with Facebook',
-                      horizontalPadding: 80,
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      'or',
-                      style: TextStyle(fontSize: 17),
-                    ),
-                    const SizedBox(height: 15),
-                    LoginField(
-                      hintiText: 'Email',
-                      controller: emailController,
-                    ),
-                    const SizedBox(height: 15),
-                    LoginField(
-                      hintiText: 'Password',
-                      controller: passwordController,
-                    ),
-                    const SizedBox(height: 20),
-                    GradientButton(
-                        onPressed: () {
-                          context.read<AuthBloc>().add(AuthLogginRequested(
-                              email: emailController.text.trim(),
-                              password: passwordController.text.trim()));
-                        },
-                        text: 'Sign in'),
-                    const SizedBox(height: 30),
-                    const Column(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Don\'t have an Account?',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            SizedBox(width: 10),
-                            Text(
-                              'Create account',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 16),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 10),
                         Text(
-                          'Forgot Password',
+                          'Don\'t have an Account?',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Create account',
                           style: TextStyle(
                               fontWeight: FontWeight.bold, fontSize: 16),
                         ),
-                        SizedBox(height: 40),
                       ],
-                    )
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Forgot Password',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    SizedBox(height: 40),
                   ],
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+                )
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
